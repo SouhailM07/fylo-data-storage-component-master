ff1e7561516b4ed3549d3bba73e4228e173deda0
shadow <lightshadow416@gmail.com>
2023-10-04 06:46:05 +0100

    [FyloPanel] component closer look {1}

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,12 +1,16 @@
 // style
 import "./container.css";
 // components
+import { FyloPanel } from "../../components";
 
 export default function Container() {
   return (
     <>
-      <div id="container" className="h-screen bg-VeryDarkBlue">
-        sb
+      <div
+        id="container"
+        className="h-screen bg-VeryDarkBlue flex items-center"
+      >
+        <FyloPanel />
       </div>
     </>
   );
--- src/components/FyloPanel/FyloPanel.tsx
+++ src/components/FyloPanel/FyloPanel.tsx
@@ -1,3 +1,32 @@
+// style
+import "./fyloPanel.css";
+// assets
+import fyloIcon from "../../assets/images/logo.svg";
+import uploadIcon from "../../assets/images/icon-upload.svg";
+import folderIcon from "../../assets/images/icon-folder.svg";
+import documentIcon from "../../assets/images/icon-document.svg";
+
+/*=======================================================================================*/
+// component section
+/*=======================================================================================*/
+
 export default function FyloPanel() {
-  return <>FyloPanel</>;
+  let arrOfIcons: string[] = [documentIcon, folderIcon, uploadIcon];
+
+  return (
+    <>
+      <div id="FyloPanel">
+        <img src={fyloIcon} alt="FyloLogo" />
+        <ul>
+          {arrOfIcons.map((e, i) => {
+            return (
+              <li key={i}>
+                <img src={e} alt="Icon Not Supported" />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
