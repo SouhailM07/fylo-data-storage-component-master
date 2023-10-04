1ec1044dfd527cae52032603ac5f3d7ac9d17d72
shadow <lightshadow416@gmail.com>
2023-10-04 06:22:39 +0100

    [Container] component was created + adding colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 /** @type {import(tailwindcss).Config} */ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        PaleBlue: "hsl(243, 100%, 93%)",
+        GrayishBlue: "hsl(229, 7%, 55%)",
+        DarkBlue: "hsl(228, 56%, 26%)",
+        VeryDarkBlue: "hsl(229, 57%, 11%)",
+      },
+    },
+  },
   plugins: [],
 };
