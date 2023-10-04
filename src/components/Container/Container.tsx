002ff24783177c460f99483ee590eb535176deb0
shadow <lightshadow416@gmail.com>
2023-10-04 08:54:43 +0100

    [StoragePanel] component closer look {1} + project closer look{1}

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,16 +1,19 @@
 // style
 import "./container.css";
 // components
-import { FyloPanel } from "../../components";
+import { FyloPanel, StoragePanel } from "../../components";
 
 export default function Container() {
   return (
     <>
       <div
         id="container"
-        className="h-screen bg-VeryDarkBlue flex items-center"
+        className="h-screen bg-VeryDarkBlue flex items-center justify-center"
       >
-        <FyloPanel />
+        <div className="flex items-center ">
+          <FyloPanel />
+          <StoragePanel />
+        </div>
       </div>
     </>
   );
--- src/components/StoragePanel/StoragePanel.tsx
+++ src/components/StoragePanel/StoragePanel.tsx
@@ -1,3 +1,40 @@
+// assets
+import "./storagePanel.css";
+
 export default function StoragePanel() {
-  return <>StoragePanel</>;
+  return (
+    <>
+      <div id="StoragePanel">
+        {/*  */}
+        <div id="StoragePanel-top">
+          <div id="StoragePanel-top-comment">
+            <div id="StoragePanel-top-comment-text">
+              <span className="text-[2.7rem] text-black mr-2">185</span>
+              <span className="mt-2">GB LEFT</span>
+            </div>
+            <div id="panelCurve"></div>
+          </div>
+          {/*  */}
+          <p id="StoragePanel-top-title">
+            You've used <span>815 GB </span>
+            of your storage
+          </p>
+        </div>
+        {/*  */}
+        <div>
+          <div>
+            <div id="StoragePanel-bottom-barContainer">
+              <div id="StoragePanel-bottom-bar">
+                <div className="w-[1rem] h-[1rem] bg-white rounded-full"></div>
+              </div>
+            </div>
+            <div id="StoragePanel-bottom-storageUnits">
+              <span>0 GB</span>
+              <span>1000 GB</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
