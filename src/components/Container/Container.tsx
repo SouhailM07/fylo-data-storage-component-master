a3f533bd88f5584a069c581925fb31229ec517ca
shadow <lightshadow416@gmail.com>
2023-10-04 09:01:38 +0100

    [Container] component is responsive now

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -6,11 +6,8 @@ import { FyloPanel, StoragePanel } from "../../components";
 export default function Container() {
   return (
     <>
-      <div
-        id="container"
-        className="h-screen bg-VeryDarkBlue flex items-center justify-center"
-      >
-        <div className="flex items-center ">
+      <div id="container">
+        <div id="flexContainer" className="flex items-center ">
           <FyloPanel />
           <StoragePanel />
         </div>
