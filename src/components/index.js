36d770cc3af95a3dc614ab1b4255426889e5eed8
shadow <lightshadow416@gmail.com>
2023-10-04 06:47:52 +0100

    [StoragePanel] component was created

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Container } from "./Container/Container";
 export { default as FyloPanel } from "./FyloPanel/FyloPanel";
+export { default as StoragePanel } from "./StoragePanel/StoragePanel";
